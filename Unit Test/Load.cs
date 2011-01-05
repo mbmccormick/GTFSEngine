e6f955c19f83a0879c329681fafebda3fc94610b
Paul Stancer <pstancer@gmail.com>
2011-01-05 17:05:18 +0000

    Added test project to correct path

---
+++ Unit Test/Load.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Stancer.GTFSEngine.Test
+{
+    [TestClass]
+    public class Load
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
